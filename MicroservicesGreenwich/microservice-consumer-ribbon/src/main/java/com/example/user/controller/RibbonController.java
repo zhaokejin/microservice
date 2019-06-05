16c98ac5661338de2aaf31f71a7b63793770c72e
zhaokejin <210006540@qq.com>
2019-06-05 17:40:23 +0800

    修改代码格式

--- MicroservicesGreenwich/microservice-consumer-ribbon/src/main/java/com/example/user/controller/RibbonController.java
+++ MicroservicesGreenwich/microservice-consumer-ribbon/src/main/java/com/example/user/controller/RibbonController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-  @RequestMapping("/ribbon")
+@RequestMapping("/ribbon")
 @RestController
 public class RibbonController {
   @Autowired
