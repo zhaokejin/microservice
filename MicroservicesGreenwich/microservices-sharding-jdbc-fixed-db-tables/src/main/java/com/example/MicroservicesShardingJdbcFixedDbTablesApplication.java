a730c2f7ce8ee443c3929a0ff6a9bb6144fe2c2e
zhaokejin <210006540@qq.com>
2019-06-18 10:43:42 +0800

    ShardingSphere使用固定值的方式来实现库分片

---
+++ MicroservicesGreenwich/microservices-sharding-jdbc-fixed-db-tables/src/main/java/com/example/MicroservicesShardingJdbcFixedDbTablesApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesShardingJdbcFixedDbTablesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesShardingJdbcFixedDbTablesApplication.class, args);
+    }
+
+}
