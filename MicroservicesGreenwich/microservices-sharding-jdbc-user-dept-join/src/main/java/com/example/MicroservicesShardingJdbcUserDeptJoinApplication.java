a825625f3214773fedbea2fddb73835c0ec1cc7e
zhaokejin <210006540@qq.com>
2019-06-18 10:44:55 +0800

    ShardingSphere使用join查询返回示例

---
+++ MicroservicesGreenwich/microservices-sharding-jdbc-user-dept-join/src/main/java/com/example/MicroservicesShardingJdbcUserDeptJoinApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesShardingJdbcUserDeptJoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesShardingJdbcUserDeptJoinApplication.class, args);
+    }
+
+}
