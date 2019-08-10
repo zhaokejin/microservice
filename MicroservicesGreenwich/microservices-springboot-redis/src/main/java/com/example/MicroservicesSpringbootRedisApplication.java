b71d41f0db2abe1a84a5d174d12c1192007a3388
zhaokejin <210006540@qq.com>
2019-08-10 17:18:10 +0800

    springboot整合操作redis

---
+++ MicroservicesGreenwich/microservices-springboot-redis/src/main/java/com/example/MicroservicesSpringbootRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSpringbootRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesSpringbootRedisApplication.class, args);
+    }
+
+}
---
+++ MicroservicesGreenwich/microservices-springboot-redis/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.service;
+
+import com.example.model.UserDomain;
+
+import java.util.List;
+
+public interface UserService {
+
+    int insert(UserDomain record);
+
+    void deleteUserById(Integer userId);
+
+    UserDomain updateUser(UserDomain userDomain);
+
+    List<UserDomain> selectUsers();
+
+    UserDomain findById(Integer id);
+}
