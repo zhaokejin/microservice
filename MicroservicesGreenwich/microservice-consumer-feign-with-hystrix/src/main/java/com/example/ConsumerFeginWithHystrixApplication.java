163533baf5dcc375acae462246a163dbba51a265
zhaokejin <zhaokejin@outlook.com>
2019-10-15 13:14:11 +0800

    修改名字规范

--- MicroservicesGreenwich/microservice-consumer-feign-with-hystrix/src/main/java/com/example/ConsumerFeginWithHystrixApplication.java
+++ MicroservicesGreenwich/microservice-consumer-feign-with-hystrix/src/main/java/com/example/ConsumerFeginWithHystrixApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class ConsumerFeginApplication {
+public class ConsumerFeginWithHystrixApplication {
   public static void main(String[] args) {
-    SpringApplication.run(ConsumerFeginApplication.class, args);
+    SpringApplication.run(ConsumerFeginWithHystrixApplication.class, args);
   }
 }
