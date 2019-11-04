703b13cbf6913eaf40ccbd514b1b7e23971f5cd5
zhaokejin <zhaokejin@outlook.com>
2019-11-04 10:16:41 +0800

    修改mongodb的规范

--- MicroservicesGreenwich/microservices-springboot-mongodb/src/main/java/com/example/config/TransactionConfig.java
+++ MicroservicesGreenwich/microservices-springboot-mongodb/src/main/java/com/example/config/TransactionConfig.java
@@ -1,9 +1,9 @@
 package com.example.config;
 
-        import org.springframework.context.annotation.Bean;
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.data.mongodb.MongoDbFactory;
-        import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 
 @Configuration
 public class TransactionConfig {
