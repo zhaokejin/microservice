07aee4749efc940c0d4dc1c964b889398a5dbc0b
zhaokejin <zhaokejin@outlook.com>
2020-01-16 16:12:51 +0800

    SpringBoot应用docker化并发布到远程服务器

---
+++ MicroservicesGreenwich/spring-boot-docker-push-server/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping(value = "test", produces = "text/plain;charset=utf-8")
+    public String authGet(String str) {
+        return "success";
+    }
+
+}
