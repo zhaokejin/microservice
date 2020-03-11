2de87ca48977d42dbf809f1f74406aec13085b0f
zhaokejin <zhaokejin@outlook.com>
2020-03-11 16:47:48 +0800

    add flowable-admin

---
+++ flowable-admin/src/main/java/cn/cicoding/FlowableAdminApplication.java
@@ -0,0 +1,17 @@
+package cn.cicoding;
+
+import org.flowable.ui.admin.conf.ApplicationConfiguration;
+import org.flowable.ui.admin.conf.DispatcherServletConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@Import({ApplicationConfiguration.class, DispatcherServletConfiguration.class})
+@SpringBootApplication
+public class FlowableAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableAdminApplication.class, args);
+    }
+
+}
