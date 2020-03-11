8f508b0ab340f47a732af02eecf3fe0ab0d775ad
zhaokejin <zhaokejin@outlook.com>
2020-03-11 15:34:07 +0800

    delete

--- MicroservicesGreenwich/microservice-mybatis-plus-custom-multiple-data-sources/src/main/java/com/example/DataSourceNames.java
+++
@@ -1,6 +0,0 @@
-package com.example;
-
-public interface DataSourceNames {
-    String FIRST = "first";
-    String SECOND = "second";
-}
--- MicroservicesGreenwich/microservice-mybatis-plus-custom-multiple-data-sources/src/main/java/com/example/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.entity.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
