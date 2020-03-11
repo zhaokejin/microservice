547ff2f8c6a8177bcb0f93008954be33c67b5b11
zhaokejin <zhaokejin@outlook.com>
2020-03-11 14:39:37 +0800

    删除，修改目录结构

--- MicroservicesGreenwich/rocketmq-producer-demo/src/main/java/com/example/rocketmqdemo/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.example.rocketmqdemo;
- 
-public interface Constant {
-    String TOPIC = "string-topic";
-    String PRODUCER_GROUP = "user_producer_group";
-    String CONSUMER_GROUP = "user_consumer_group";
-}
