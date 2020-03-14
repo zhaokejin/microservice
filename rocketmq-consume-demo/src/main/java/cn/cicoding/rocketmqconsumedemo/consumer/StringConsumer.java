7dca7b760c08cdbbbd7738edaf9f1b61e5593343
zhaokejin <zhaokejin@outlook.com>
2020-03-14 21:08:11 +0800

    **RocketMQ消费者demo**

---
+++ rocketmq-consume-demo/src/main/java/cn/cicoding/rocketmqconsumedemo/consumer/StringConsumer.java
@@ -0,0 +1,17 @@
+package cn.cicoding.rocketmqconsumedemo.consumer;
+
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * RocketMQMessageListener
+ */
+@Service
+@RocketMQMessageListener(topic = "${demo.rocketmq.topic}", consumerGroup = "string_consumer")
+public class StringConsumer implements RocketMQListener<String> {
+    @Override
+    public void onMessage(String message) {
+        System.out.printf("------- StringConsumer received: %s \n", message);
+    }
+}
