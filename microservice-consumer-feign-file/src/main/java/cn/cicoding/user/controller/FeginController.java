b8b41ee5c6e911c650c7c57e088e751beae48a9c
zhaokejin <zhaokejin@outlook.com>
2020-03-17 21:32:57 +0800

    feign上传文件服务

---
+++ microservice-consumer-feign-file/src/main/java/cn/cicoding/user/controller/FeginController.java
@@ -0,0 +1,27 @@
+package cn.cicoding.user.controller;
+
+import cn.cicoding.user.entity.User;
+import cn.cicoding.user.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequestMapping("/fegin")
+@RestController
+public class FeginController {
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Integer id) {
+        return this.userFeignClient.findById(id);
+    }
+
+  @PostMapping("/upload")
+  @ResponseBody
+  public String upload(@RequestParam("file") MultipartFile file) {
+    userFeignClient.handleFileUpload(file);
+    return "success";
+  }
+
+}
