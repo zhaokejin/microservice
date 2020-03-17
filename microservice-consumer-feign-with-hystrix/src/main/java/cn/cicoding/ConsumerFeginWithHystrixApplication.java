1cae851cba3baa5fdd03e739fcd20bf8de0c3401
zhaokejin <zhaokejin@outlook.com>
2020-03-17 18:00:55 +0800

    **Fegin使用hystrix**

---
+++ microservice-consumer-feign-with-hystrix/src/main/java/cn/cicoding/ConsumerFeginWithHystrixApplication.java
@@ -0,0 +1,13 @@
+package cn.cicoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class ConsumerFeginWithHystrixApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerFeginWithHystrixApplication.class, args);
+  }
+}
---
+++ microservice-consumer-feign-with-hystrix/src/main/java/cn/cicoding/user/feign/UserFeignClient.java
@@ -0,0 +1,23 @@
+package cn.cicoding.user.feign;
+
+import cn.cicoding.user.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "microservice-provider", fallback = UserFeignClientFallback.class)
+public interface UserFeignClient {
+  @GetMapping("/user/{id}")
+  User findById(@PathVariable("id") Integer id);
+}
+
+
+
+@Component
+class UserFeignClientFallback implements UserFeignClient {
+  @Override
+  public User findById(Integer id) {
+    return new User(id, "默认用户", "aaaaaa", "13899988898");
+  }
+}
