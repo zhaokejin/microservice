e6b39ce8247d875608d0dbd20d257534bfd8455c
zhaokejin <zhaokejin@outlook.com>
2020-03-17 18:02:24 +0800

    **Feign暴露`/actuaotr/hystrix.stream`端点**

---
+++ microservice-consumer-movie-feign-hystrix-stream/src/main/java/cn/cicoding/cloud/microserviceconsumermoviefeignhystrixstream/user/entity/User.java
@@ -0,0 +1,18 @@
+package cn.cicoding.cloud.microserviceconsumermoviefeignhystrixstream.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+  private Integer userId;
+
+  private String userName;
+
+  private String password;
+
+  private String phone;
+}
