c2d6d612129532f5cab5f40a7e52f45171c1b011
zhaokejin <zhaokejin@outlook.com>
2020-03-19 11:25:16 +0800

    flowable微服务 集成

---
+++ microservice-flowable/src/main/java/cn/cicoding/config/FlowableConfig.java
@@ -0,0 +1,24 @@
+package cn.cicoding.config;
+
+import org.flowable.spring.SpringProcessEngineConfiguration;
+import org.flowable.spring.boot.EngineConfigurationConfigurer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈${DESCRIPTION}〉
+ *
+ * @author zhaokejin
+ * @create 2019-5-13 11:35:51
+ */
+@Configuration
+public class FlowableConfig implements EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
+
+
+	@Override
+	public void configure(SpringProcessEngineConfiguration engineConfiguration) {
+		engineConfiguration.setActivityFontName("宋体");
+		engineConfiguration.setLabelFontName("宋体");
+		engineConfiguration.setAnnotationFontName("宋体");
+	}
+}
