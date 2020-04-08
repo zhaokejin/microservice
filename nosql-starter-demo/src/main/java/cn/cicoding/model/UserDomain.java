376d8da17f555e548f3d472d504de0177a2d7d83
zhaokejin <zhaokejin@outlook.com>
2020-04-08 11:12:55 +0800

    nosql starter  demo test

---
+++ nosql-starter-demo/src/main/java/cn/cicoding/model/UserDomain.java
@@ -0,0 +1,46 @@
+package cn.cicoding.model;
+
+import java.io.Serializable;
+
+public class UserDomain implements Serializable {
+
+    private Integer userId;
+
+    private String userName;
+
+    private String password;
+
+    private String phone;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
