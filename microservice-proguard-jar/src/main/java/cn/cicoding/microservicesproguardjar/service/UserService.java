56a7cba6f6e6d5fd10499dd932396a79771e33de
zhaokejin <zhaokejin@outlook.com>
2020-11-25 21:06:00 +0800

    添加jar混淆

---
+++ microservice-proguard-jar/src/main/java/cn/cicoding/microservicesproguardjar/service/UserService.java
@@ -0,0 +1,19 @@
+package cn.cicoding.microservicesproguardjar.service;
+
+
+import cn.cicoding.microservicesproguardjar.model.UserDomain;
+
+import java.util.List;
+
+public interface UserService {
+
+    int insert(UserDomain record);
+
+    void deleteUserById(Integer userId);
+
+    void updateUser(UserDomain userDomain);
+
+    List<UserDomain> selectUsers();
+
+    UserDomain findById(Integer id);
+}
