151f33bc382b512962ad1d6bcc9c16ab0d01bc2f
zhaokejin <zhaokejin@outlook.com>
2020-12-05 21:31:49 +0800

    flowable-idm

---
+++ spring-cloud-greenwith/flowable-idm/src/main/java/cn/cicoding/FlowableIdmApplication.java
@@ -0,0 +1,17 @@
+package cn.cicoding;
+
+import org.flowable.ui.idm.conf.ApplicationConfiguration;
+import org.flowable.ui.idm.servlet.AppDispatcherServletConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@Import({ApplicationConfiguration.class, AppDispatcherServletConfiguration.class})
+@SpringBootApplication
+public class FlowableIdmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableIdmApplication.class, args);
+    }
+
+}
