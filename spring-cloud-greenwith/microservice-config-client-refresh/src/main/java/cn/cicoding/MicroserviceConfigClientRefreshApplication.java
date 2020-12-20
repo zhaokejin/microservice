9941590608ab8125c8ddd535792ad827a40f74f7
zhaokejin <zhaokejin@outlook.com>
2020-12-20 21:05:01 +0800

    microservice-config-client-refresh

---
+++ spring-cloud-greenwith/microservice-config-client-refresh/src/main/java/cn/cicoding/MicroserviceConfigClientRefreshApplication.java
@@ -0,0 +1,13 @@
+package cn.cicoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigClientRefreshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConfigClientRefreshApplication.class, args);
+    }
+
+}
