5fc0c6de72f3de810070b227633f719fcc10ef15
zhaokejin <zhaokejin@outlook.com>
2021-01-20 22:24:14 +0800

    microservice-mybatis-plus-custom-multiple-data-sources

---
+++ spring-cloud-greenwith/microservice-mybatis-plus-custom-multiple-data-sources/src/main/java/cn/cicoding/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package cn.cicoding.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import cn.cicoding.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
