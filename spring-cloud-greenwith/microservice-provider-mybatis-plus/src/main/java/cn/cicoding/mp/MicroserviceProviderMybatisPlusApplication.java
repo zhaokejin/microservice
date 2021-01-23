2cc17f42365c516d6eea33fcfa6f84414e3396e7
zhaokejin <zhaokejin@outlook.com>
2021-01-23 21:45:03 +0800

    microservice-provider-mybatis-plus

---
+++ spring-cloud-greenwith/microservice-provider-mybatis-plus/src/main/java/cn/cicoding/mp/MicroserviceProviderMybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package cn.cicoding.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProviderMybatisPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProviderMybatisPlusApplication.class, args);
+	}
+
+}
