27682ffb68a11d5eac93970dd7a765750d2aec4e
zhaokejin <zhaokejin@outlook.com>
2021-03-10 22:55:28 +0800

    shariding demo

---
+++ spring-cloud-greenwith/microservice-sharding-jdbc-db-tables/src/main/java/cn/cicoding/service/UserService.java
@@ -0,0 +1,17 @@
+package cn.cicoding.service;
+
+import cn.cicoding.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+	List<User> list();
+	
+	Long add(User user);
+	
+	User findById(Long id);
+	
+	User findByName(String name);
+	
+}
