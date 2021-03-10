bf1415e662b0961ba51ff33472996f0f6c9ffd7e
zhaokejin <zhaokejin@outlook.com>
2021-03-10 22:44:20 +0800

    sharding-jdbc demo

---
+++ spring-cloud-greenwith/microservice-sharding-jdbc/src/main/java/cn/cicoding/MicroservicesShardingJdbcApplication.java
@@ -0,0 +1,13 @@
+package cn.cicoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesShardingJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesShardingJdbcApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-greenwith/microservice-sharding-jdbc/src/main/java/cn/cicoding/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package cn.cicoding.service.impl;
+
+import cn.cicoding.dao.UserDaoMapper;
+import cn.cicoding.service.UserService;
+import cn.cicoding.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserDaoMapper userDao;
+	
+	public List<User> list() {
+		List<User> list = userDao.list();
+		return list;
+	}
+
+	public Long add(User user) {
+		return userDao.addUser(user);
+	}
+
+	@Override
+	public User findById(Long id) {
+		return userDao.findById(id);
+	}
+
+	@Override
+	public User findByName(String name) {
+		return userDao.findByName(name);
+	}
+
+	@Override
+	public Long clean() {
+		return userDao.clean();
+	}
+
+}
