db1f4d3b24e61c5e218f900a7391f2cfeb5b1d5b
zhaokejin <zhaokejin@outlook.com>
2021-03-10 22:56:53 +0800

    springboot  MongoDB

---
+++ spring-cloud-greenwith/microservice-springboot-mongodb/src/main/java/cn/cicoding/dao/UserDao.java
@@ -0,0 +1,21 @@
+package cn.cicoding.dao;
+
+import cn.cicoding.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface UserDao {
+
+    int insert(User record);
+
+    void deleteUserById(@Param("userId") Integer userId);
+
+    void updateUser(User userDomain);
+
+    List<User> selectUsers();
+
+    User findById(Integer id);
+}
