c89f1354e52f51d416c3bccf538da340e775ffd5
zhaokejin <zhaokejin@outlook.com>
2021-06-07 20:52:02 +0800

    打印spring的相关包路径

---
+++ spring-cloud-greenwith/spring-annotation/src/main/java/cn/cicoding/SpringAnnotationApplication.java
@@ -0,0 +1,13 @@
+package cn.cicoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnnotationApplication.class, args);
+	}
+
+}
