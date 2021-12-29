87b34584be4ab06f92139d2a08463e73b32fc6a6
zhaokejin <zhaokejin@outlook.com>
2021-12-29 16:00:51 +0800

    微服务示例 jpa服务提供者

---
+++ spring-cloud-greenwith/microservice-simple-provider-user/src/main/java/cn/cicoding/cloud/controller/UserController.java
@@ -0,0 +1,24 @@
+package cn.cicoding.cloud.controller;
+
+import cn.cicoding.cloud.entity.User;
+import cn.cicoding.cloud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public Optional<User> findById(@PathVariable Long id) {
+        return this.userRepository.findById(id);
+    }
+}
