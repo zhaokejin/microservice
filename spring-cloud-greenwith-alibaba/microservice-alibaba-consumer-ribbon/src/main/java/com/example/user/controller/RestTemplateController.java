c9ea891113bbbf25ded92e146f5cd43d064f938b
zhaokejin <zhaokejin@outlook.com>
2022-10-16 21:51:33 +0800

    springcloud alibaba consumer ribbon

---
+++ spring-cloud-greenwith-alibaba/microservice-alibaba-consumer-ribbon/src/main/java/com/example/user/controller/RestTemplateController.java
@@ -0,0 +1,25 @@
+package com.example.user.controller;
+
+import com.example.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RequestMapping("/rest")
+@RestController
+public class RestTemplateController {
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @GetMapping("/user/{id}")
+  public User findById(@PathVariable Long id) {
+    // 这里用到了RestTemplate的占位符能力
+    User user = this.restTemplate.getForObject("http://microservice-provider/user/{id}", User.class, id);
+//    User user = this.restTemplate.getForObject("http://localhost:8080/user/{id}", User.class, id);
+    // ...微服务的业务...
+    return user;
+  }
+}
