d17257ff39d7ecbe962d2dc34a10033c000fcd5c
zhaokejin <zhaokejin@outlook.com>
2022-10-23 22:39:59 +0800

    spring cloud gateway

---
+++ spring-cloud-alibaba/spring-boot-alibaba-gateway/src/main/java/cn/cicoding/gateway/SpringBootAlibabaGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.cicoding.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAlibabaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAlibabaGatewayApplication.class, args);
+    }
+
+}
