90e6468adccaf25b76e080f33cb9c5f7c4860e31
zhaokejin <zhaokejin@outlook.com>
2023-01-04 09:24:13 +0800

    wq 登录

---
+++ spring-boot-example/security-login-db-and-qq/src/main/java/com/cicoding/exception/ApplicationRuntimeException.java
@@ -0,0 +1,37 @@
+package com.cicoding.exception;
+
+/***
+ * 程序-运行时错误
+ *
+ * @author zhaokejin
+ *
+ */
+public class ApplicationRuntimeException extends RuntimeException {
+	
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -3607242177736229275L;
+	
+	public ApplicationRuntimeException(String mes) {
+		super(mes);
+	}
+	
+	public ApplicationRuntimeException() {
+		super();
+	}
+	
+	public ApplicationRuntimeException(String message, Throwable cause, boolean enableSuppression,
+									   boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+	
+	public ApplicationRuntimeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public ApplicationRuntimeException(Throwable cause) {
+		super(cause);
+	}
+	
+}
