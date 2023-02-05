65c1073df0fc911bfcfa20b7f02c999b54e3ea42
zhaokejin <zhaokejin@outlook.com>
2023-02-05 22:19:56 +0800

    jsr add commit

---
+++ spring-boot-example/springboot-jsr-303/src/main/java/cn/cicoding/springbootjsr303/SpringbootJsr303Application.java
@@ -0,0 +1,14 @@
+package cn.cicoding.springbootjsr303;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableSwagger2Doc
+@SpringBootApplication
+public class SpringbootJsr303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJsr303Application.class, args);
+    }
+
+}
