cfc893589e7c733b63bfc4f6194b76094895e509
zhaokejin <zhaokejin@outlook.com>
2023-02-05 08:57:10 +0800

    json 提交

---
+++ spring-boot-example/springboot-json/src/main/java/cn/cicoding/SpringbootJsonApplication.java
@@ -0,0 +1,13 @@
+package cn.cicoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJsonApplication.class, args);
+	}
+
+}
---
+++ spring-boot-example/springboot-json/src/main/java/cn/cicoding/entity/User.java
@@ -0,0 +1,33 @@
+package cn.cicoding.entity;
+
+/**
+ * @author zhaokejin
+ * @description
+ * @date 2020/2/3
+ */
+public class User {
+	/**
+	 * 昵称
+	 */
+	private String nickname;
+
+	/**
+	 * 性别
+	 */
+	private String gender;
+
+	/**
+	 * 省份
+	 */
+	private String province;
+
+	/**
+	 * 出生年
+	 */
+	private String year;
+
+	/**
+	 * 头像
+	 */
+	private String avatar;
+}
