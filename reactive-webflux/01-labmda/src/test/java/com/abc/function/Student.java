b75417f758421c1968219c3061807cf3cc5e264b
zhaokejin <zhaokejin@outlook.com>
2023-02-08 21:28:14 +0800

    webflux代码提交

---
+++ reactive-webflux/01-labmda/src/test/java/com/abc/function/Student.java
@@ -0,0 +1,13 @@
+package com.abc.function;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private int age;
+}
