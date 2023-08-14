0f838c3e60873bbebfe7ccc28a2d4d38fb52eb3e
zhaokejin <zhaokejin@outlook.com>
2023-08-14 15:42:04 +0800

    springboot整合zk

---
+++ spring-boot-example/springboot-zookeeper-curator/src/main/java/cn/cicoding/SpringbootZookeeperCuratorApplication.java
@@ -0,0 +1,13 @@
+package cn.cicoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootZookeeperCuratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootZookeeperCuratorApplication.class, args);
+    }
+
+}
