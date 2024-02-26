5084d08cff4eab61eede2a481863eb7b8f1bb787
zhaokejin <zhaokejin@outlook.com>
2024-02-26 17:12:14 +0800

    canal redis springboot 集成

---
+++ spring-boot-example/springboot-canal-redis/src/main/java/cn/cicoding/canal/SpringbootCanalRedisApplication.java
@@ -0,0 +1,13 @@
+package cn.cicoding.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCanalRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCanalRedisApplication.class, args);
+    }
+
+}
