1c8e61aeda053af8ffddaa68874631f94bc58e71
zhaokejin <zhaokejin@outlook.com>
2024-02-26 08:09:41 +0800

    303 启动配置

--- spring-boot-example/springboot-jsr-303/src/main/java/cn/cicoding/springbootjsr303/SpringbootJsr303Application.java
+++ spring-boot-example/springboot-jsr-303/src/main/java/cn/cicoding/springbootjsr303/SpringbootJsr303Application.java
@@ -1,9 +1,11 @@
 package cn.cicoding.springbootjsr303;
 
-import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@EnableSwagger2Doc
+
+/**
+ * https://github.com/SpringForAll/spring-boot-starter-swagger
+ */
 @SpringBootApplication
 public class SpringbootJsr303Application {
 
