4aa1e03ab12f48f27e043a23438f9bb83b8110e1
zhaokejin <zhaokejin@outlook.com>
2024-07-30 08:27:09 +0800

    升级版本

--- spring-boot-example/springboot-introduction/src/test/java/cn/cicoding/springbootintroduction/SpringbootIntroductionApplicationTests.java
+++ spring-boot-example/springboot-introduction/src/test/java/cn/cicoding/springbootintroduction/SpringbootIntroductionApplicationTests.java
@@ -1,7 +1,5 @@
 package cn.cicoding.springbootintroduction;
 
-import org.junit.Before;
-import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,11 +13,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class SpringbootIntroductionApplicationTests {
 
     private MockMvc mvc;
-    @Before
     public void setUp() throws Exception {
         mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
     }
-    @Test
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
