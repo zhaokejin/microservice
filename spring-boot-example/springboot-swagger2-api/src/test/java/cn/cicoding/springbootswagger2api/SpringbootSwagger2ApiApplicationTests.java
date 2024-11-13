01c7d7d198e932fdbbb0ed5b43c65b644142a38b
zhaokejin <zhaokejin@outlook.com>
2024-11-13 08:58:57 +0800

    修改代码逻辑

--- spring-boot-example/springboot-swagger2-api/src/test/java/cn/cicoding/springbootswagger2api/SpringbootSwagger2ApiApplicationTests.java
+++ spring-boot-example/springboot-swagger2-api/src/test/java/cn/cicoding/springbootswagger2api/SpringbootSwagger2ApiApplicationTests.java
@@ -1,6 +1,6 @@
 package cn.cicoding.springbootswagger2api;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
