f0c5a94d82c58f6b95447c774781c6d3964b54cf
zhaokejin <zhaokejin@outlook.com>
2025-01-03 22:35:10 +0800

    page config

---
+++ spring-boot-example/springboot-mybatis-pagehelper/src/main/java/com/example/dao/UserDao.java
@@ -0,0 +1,21 @@
+package com.example.dao;
+
+import com.example.model.UserDomain;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface UserDao {
+
+    int insert(UserDomain record);
+
+    void deleteUserById(@Param("userId") Integer userId);
+
+    void updateUser(UserDomain userDomain);
+
+    List<UserDomain> selectUsers();
+
+    UserDomain findById(Integer id);
+}
