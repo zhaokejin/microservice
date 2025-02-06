45b6dd2d4619a305fc14be119a4878a54dfd1198
zhaokejin <zhaokejin@outlook.com>
2025-02-06 13:37:37 +0800

    有赞延时消息设计

---
+++ spring-boot-example/springboot-redis-like-delay-message/src/main/java/cn/cicoding/delay/bean/DelayJob.java
@@ -0,0 +1,44 @@
+package cn.cicoding.delay.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 延迟任务
+ * @author cicoding.cn
+ * @date 2021-07-25 15:24
+ **/
+@Data
+@AllArgsConstructor
+public class DelayJob implements Serializable {
+
+
+    /**
+     * 延迟任务的唯一标识
+     */
+    private long jodId;
+    
+    /**
+     * 任务的执行时间
+     */
+    private long delayDate;
+
+    /**
+     * 任务类型（具体业务类型）
+     */
+    private String topic;
+
+
+    public DelayJob(Job job) {
+        this.jodId = job.getId();
+        this.delayDate = System.currentTimeMillis() + job.getDelayTime();
+        this.topic = job.getTopic();
+    }
+
+    public DelayJob(Object value, Double score) {
+        this.jodId = Long.parseLong(String.valueOf(value));
+        this.delayDate = System.currentTimeMillis() + score.longValue();
+    }
+}
