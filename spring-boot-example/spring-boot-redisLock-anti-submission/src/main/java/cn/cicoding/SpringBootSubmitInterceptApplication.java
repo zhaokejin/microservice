eb365ed86373cf2fc0cee8097bb4c2e58abf4d55
zhaokejin <zhaokejin@outlook.com>
2025-02-07 16:22:07 +0800

    redis 分布式锁实现

---
+++ spring-boot-example/spring-boot-redisLock-anti-submission/src/main/java/cn/cicoding/SpringBootSubmitInterceptApplication.java
@@ -0,0 +1,32 @@
+package cn.cicoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author cicoding.cn
+ * @description
+ * @date 2021-09-04
+ */
+@SpringBootApplication
+public class SpringBootSubmitInterceptApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSubmitInterceptApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        return factory;
+    }
+
+}
