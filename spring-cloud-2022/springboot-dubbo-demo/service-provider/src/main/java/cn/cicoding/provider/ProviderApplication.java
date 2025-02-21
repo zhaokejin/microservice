b384d473907dae7012261b1f073e2cdf610d3cfa
zhaokejin <zhaokejin@outlook.com>
2025-02-21 16:13:06 +0800

    dobbo demo uodate

---
+++ spring-cloud-2022/springboot-dubbo-demo/service-provider/src/main/java/cn/cicoding/provider/ProviderApplication.java
@@ -0,0 +1,18 @@
+package cn.cicoding.provider;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Neo.Zzj
+ * @date 2020/12/18
+ */
+@EnableDubbo
+@SpringBootApplication
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}
