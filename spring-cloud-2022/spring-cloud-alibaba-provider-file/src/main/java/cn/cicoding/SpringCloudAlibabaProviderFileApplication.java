e83bb88853879f995f8a0fdb33d52c498fab501e
zhaokejin <zhaokejin@outlook.com>
2025-02-28 13:54:21 +0800

    provider file commit

---
+++ spring-cloud-2022/spring-cloud-alibaba-provider-file/src/main/java/cn/cicoding/SpringCloudAlibabaProviderFileApplication.java
@@ -0,0 +1,13 @@
+package cn.cicoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaProviderFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaProviderFileApplication.class, args);
+    }
+
+}
