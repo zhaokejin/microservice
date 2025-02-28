2079284ed848f9714481e6e6cbd1f61c37a93caa
zhaokejin <zhaokejin@outlook.com>
2025-02-28 13:59:12 +0800

    config demo

---
+++ spring-cloud-greenwith-alibaba/microservice-alibaba-config/src/main/java/com/example/boot/ConfigAlibabaApplication.java
@@ -0,0 +1,12 @@
+package com.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigAlibabaApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigAlibabaApplication.class, args);
+  }
+
+}
