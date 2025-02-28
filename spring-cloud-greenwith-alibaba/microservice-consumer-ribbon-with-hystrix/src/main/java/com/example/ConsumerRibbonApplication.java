74e0073d201c60318ae65ac84107a6a727ffe333
zhaokejin <zhaokejin@outlook.com>
2025-02-28 13:56:13 +0800

    tibbon hystrix

---
+++ spring-cloud-greenwith-alibaba/microservice-consumer-ribbon-with-hystrix/src/main/java/com/example/ConsumerRibbonApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class ConsumerRibbonApplication {
+  @Bean
+  @LoadBalanced
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerRibbonApplication.class, args);
+  }
+
+}
