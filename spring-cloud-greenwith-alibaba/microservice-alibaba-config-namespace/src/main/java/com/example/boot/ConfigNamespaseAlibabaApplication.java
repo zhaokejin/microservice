e33174bc9a34ec04d82586f2bd52a99431fee9a5
zhaokejin <zhaokejin@outlook.com>
2025-03-04 08:27:40 +0800

    namespace配置

---
+++ spring-cloud-greenwith-alibaba/microservice-alibaba-config-namespace/src/main/java/com/example/boot/ConfigNamespaseAlibabaApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigNamespaseAlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigNamespaseAlibabaApplication.class, args);
+    }
+
+}
