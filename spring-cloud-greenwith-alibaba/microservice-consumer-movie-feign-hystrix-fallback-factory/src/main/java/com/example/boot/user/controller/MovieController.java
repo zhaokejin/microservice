2fed8bcb6f85f2e34778a258da5f2c7649ffb89f
zhaokejin <zhaokejin@outlook.com>
2025-03-04 08:10:00 +0800

    消费者feign

---
+++ spring-cloud-greenwith-alibaba/microservice-consumer-movie-feign-hystrix-fallback-factory/src/main/java/com/example/boot/user/controller/MovieController.java
@@ -0,0 +1,24 @@
+package com.example.boot.user.controller;
+
+import com.example.boot.user.entity.User;
+import com.example.boot.user.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zkj
+ */
+@RequestMapping("/fegin")
+@RestController
+public class MovieController {
+  @Autowired
+  private UserFeignClient userFeignClient;
+
+  @GetMapping("/user/{id}")
+  public User findById(@PathVariable Integer id) {
+    return this.userFeignClient.findById(id);
+  }
+}
