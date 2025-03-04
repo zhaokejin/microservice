eb564ceb35e13c1806c438e2a1202cb3ccf83735
zhaokejin <zhaokejin@outlook.com>
2025-03-04 08:25:58 +0800

    配置分组

---
+++ spring-cloud-greenwith-alibaba/microservice-alibaba-config-group/src/main/java/com/example/boot/ConfigGroupAlibabaApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigGroupAlibabaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigGroupAlibabaApplication.class, args);
+  }
+
+}
