1d60ee945c0bcb77b56da0331e8712879f61a856
zhaokejin <zhaokejin@outlook.com>
2025-03-04 08:22:46 +0800

    microservice-alibaba-config-dataId-profiles

---
+++ spring-cloud-greenwith-alibaba/microservice-alibaba-config-dataId-profiles/src/main/java/com/example/boot/ConfigDataidProfilesAlibabaApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigDataidProfilesAlibabaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigDataidProfilesAlibabaApplication.class, args);
+  }
+
+}
