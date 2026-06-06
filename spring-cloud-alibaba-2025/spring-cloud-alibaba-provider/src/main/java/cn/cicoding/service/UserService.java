4a4d16212d6e3f7d64102dcf23eb47bf5df233b1
zhaokejin <zhaokejin@outlook.com>
2026-06-06 09:42:08 +0800

    feat(user): 添加用户管理CRUD功能,服务生产者
    - 创建UserController实现用户增删改查REST接口
    - 实现UserDomain实体类映射数据库字段
    - 创建UserDao数据访问接口和UserMapper.xml映射文件
    - 实现UserService业务逻辑层接口
    - 集成MyBatis操作MySQL数据库
    - 配置Nacos服务注册发现功能
    - 添加用户登录认证和手机号字段支持

---
+++ spring-cloud-alibaba-2025/spring-cloud-alibaba-provider/src/main/java/cn/cicoding/service/UserService.java
@@ -0,0 +1,18 @@
+package cn.cicoding.service;
+
+import cn.cicoding.model.UserDomain;
+
+import java.util.List;
+
+public interface UserService {
+
+    int insert(UserDomain record);
+
+    void deleteUserById(Integer userId);
+
+    void updateUser(UserDomain userDomain);
+
+    List<UserDomain> selectUsers();
+
+    UserDomain findById(Integer id);
+}
