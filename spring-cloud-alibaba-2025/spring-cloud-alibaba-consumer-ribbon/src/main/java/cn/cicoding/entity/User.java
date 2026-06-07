99630b62207d89167255b80ec700be9e945918c6
zhaokejin <zhaokejin@outlook.com>
2026-06-07 10:17:23 +0800

    feat(ribbon): 添加基于RestTemplate的服务消费者功能
    - 创建RestTemplateController实现服务间调用
    - 添加User实体类用于数据传输
    - 配置LoadBalanced注解的RestTemplate Bean
    - 设置Nacos服务注册与发现配置
    - 集成Spring Cloud LoadBalancer实现负载均衡
    - 配置应用端口和管理端点暴露

---
+++ spring-cloud-alibaba-2025/spring-cloud-alibaba-consumer-ribbon/src/main/java/cn/cicoding/entity/User.java
@@ -0,0 +1,15 @@
+package cn.cicoding.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String userName;
+    private String password;
+    private String phone;
+}
